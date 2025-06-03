fa88cf1f62eb053e69d345fc21aea7134c03ae3c
Preet <mandhwanipreet@gmail.com>
2025-06-03 12:11:04 +0500

    Feature: add POST and DELETE routes

--- index.js
+++ index.js
@@ -2,18 +2,24 @@ const express = require("express")
 const mongoose = require('mongoose')
 const { title } = require("process")
 const schema = require('Schema')
+const Book = require('book')
 
 const app = express()
 
 const dbURI = 'mongodb+srv://Preet:Preet123@cluster0.xevdvdo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
  mongoose.connect(dbURI)
+ .then(()=>{
+    app.listen(8081)
+ })
+ .catch((err)=>{
+    console.log(err)
+ })
 
  const bookSchema = new createSchema ({
     title : String ,
     author : String 
  })
  app.get('/books', (req, res)=> {
-
     then((req, res )=>{
         console.log('Get all books')
     })
@@ -21,6 +27,28 @@ const dbURI = 'mongodb+srv://Preet:Preet123@cluster0.xevdvdo.mongodb.net/?retryW
         console.log(err)
      })
  })
- app.getElementByID('/books/id:', ()=>{
+ app.get('/books/id:', ()=>{
+    Book.find()
+    Book.save()
     
+ })
+ const schema = bookSchema({
+    title : 'NewBook',
+    author : 'XYZ'
+ })
+
+ app.post('/books', (req, res)=>{
+    const newbook = req.body;
+    res.send('NEW BOOK ADDED')
+ })
+
+ app.delete('/books/id:', (req, res)=>{
+    const id = req.params.id;
+    
+     then((req, res )=>{
+        console.log('Get all books')
+    })
+     .catch((err)=>{
+        console.log(err)
+     })
  })
