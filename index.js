a7c8bb499395817e83cf9397803f7d9df0a555c6
Preet <mandhwanipreet@gmail.com>
2025-06-03 11:52:33 +0500

    Feature: add book schema and GET routes

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const express = require("express")
 const mongoose = require('mongoose')
 const { title } = require("process")
+const schema = require('Schema')
+
 const app = express()
 
 const dbURI = 'mongodb+srv://Preet:Preet123@cluster0.xevdvdo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
  mongoose.connect(dbURI)
- 
+
+ const bookSchema = new createSchema ({
+    title : String ,
+    author : String 
+ })
+ app.get('/books', (req, res)=> {
+
+    then((req, res )=>{
+        console.log('Get all books')
+    })
+     .catch((err)=>{
+        console.log(err)
+     })
+ })
+ app.getElementByID('/books/id:', ()=>{
+    
+ })
